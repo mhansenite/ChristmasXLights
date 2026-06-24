bdb3fb515ee0065a5b5e261d2d8b31454414ffc0
Michael Hansen <49969626+mhansenite@users.noreply.github.com>
2026-06-23 22:21:06 -0600

    Adding in Kpop

---
+++ Shaders/Kpop/Creation by Silexars.fs
@@ -0,0 +1,94 @@
+/*
+	{
+	"DESCRIPTION": "Creation by Silexars",
+	"CATEGORIES": 
+		[
+		"generator"
+		],
+	"ISFVSN": "2",
+	"CREDIT": "ISF Import by: Old Salt",
+	"VSN": "1.0",
+	"INPUTS":
+		[
+			{
+			"NAME": "uC1",
+			"TYPE": "color",
+			"DEFAULT":[0.0,1.0,0.0,1.0]
+			},
+			{
+			"NAME": "uC2",
+			"TYPE": "color",
+			"DEFAULT":[0.0,0.0,1.0,1.0]
+			},
+			{
+			"NAME": "uC3",
+			"TYPE": "color",
+			"DEFAULT":[1.0,0.0,0.0,1.0]
+			},
+			{
+			"LABEL": "Offset: ",
+			"NAME": "uOffset",
+			"TYPE": "point2D",
+			"MAX": [1.0,1.0],
+			"MIN": [-1.0,-1.0],
+			"DEFAULT": [0.0,0.0]
+			},
+			{
+			"LABEL": "Color Mode: ",
+			"LABELS":
+				[
+				"Shader Defaults ",
+				"Alternate Color Palette (3 used) "
+				],
+			"NAME": "uColMode",
+			"TYPE": "long",
+			"VALUES": [0,1],
+			"DEFAULT": 0
+			},
+			{
+			"LABEL": "Intensity: ",
+			"NAME": "uIntensity",
+			"TYPE": "float",
+			"MAX": 4.0,
+			"MIN": 0,
+			"DEFAULT": 1.0
+			}
+		]
+	}
+*/
+// Import from: https://www.shadertoy.com/view/XsXXDn
+// Creation by Silexars
+// Credit to: 'Danilo Guanabara'
+
+
+void main()
+	{
+	
+/**** Start of Core Imported Shader Code *****/
+	vec3 c;
+	float l,z=TIME;
+	for(int i=0;i<3;i++)
+		{
+		vec2 uv, p=(gl_FragCoord.xy/RENDERSIZE) - ((uOffset+1.0)/2.0 - 0.5);
+		uv=p;
+		p-=.5;
+		p.x*=RENDERSIZE.x/RENDERSIZE.y;
+		z+=.07;
+		l=length(p);
+		uv+=p/l*(sin(z)+1.)*abs(sin(l*9.-z*2.));
+		c[i]=.01/length(abs(mod(uv,1.)-.5));
+		}
+/****  End of Core Imported Shader Code  *****/
+
+	vec4 cShad = vec4(c/l,TIME);  
+	vec3 cOut = cShad.rgb;
+	if (uColMode == 1)
+		{
+		cOut = uC1.rgb * cShad.r;
+		cOut += uC2.rgb * cShad.g;
+		cOut += uC3.rgb * cShad.b;
+		}
+	cOut = cOut * uIntensity;
+	cOut = clamp(cOut, vec3(0.0), vec3(1.0));
+	gl_FragColor = vec4(cOut.rgb,cShad.a);
+	}
